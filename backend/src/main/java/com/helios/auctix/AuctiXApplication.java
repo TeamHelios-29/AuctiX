ad15215baa4b90d7dfdd0e6f808b5a46184452a6
Sachintha Lakmin <68807141+sachintha-lk@users.noreply.github.com>
2025-01-04 16:36:16 +0530

    Initialized spring boot backend

---
+++ backend/src/main/java/com/helios/auctix/AuctiXApplication.java
@@ -0,0 +1,13 @@
+package com.helios.auctix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctiXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctiXApplication.class, args);
+	}
+
+}
