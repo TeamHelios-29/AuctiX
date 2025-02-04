691dff0902502a7ebc7c93d283a1940444ff1a77
Sakindu Ransindu <sakindu.ransindu@gmail.com>
2025-02-04 15:37:38 +0530

    setup flyway

--- backend/src/main/java/com/helios/auctix/AuctiXApplication.java
+++ backend/src/main/java/com/helios/auctix/AuctiXApplication.java
@@ -1,22 +1,34 @@
 package com.helios.auctix;
 
+import com.helios.auctix.repositories.UserRepository;
 import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import javax.sql.DataSource;
 
 @SpringBootApplication
 @Log
-public class AuctiXApplication {
+public class AuctiXApplication implements CommandLineRunner {
 
-	private final DataSource dataSource;
+	private final UserRepository userRepository;
 
-	public AuctiXApplication(final DataSource dataSource) {
-		this.dataSource = dataSource;
+	public AuctiXApplication( UserRepository userRepository) {
+		this.userRepository = userRepository;
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuctiXApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception{
+
+		System.out.println("Starting aplication...");
+		userRepository.findAll().forEach(abc->{
+			System.out.println("user info: " + abc.toString());
+		});
+
 	}
 
 }
---
+++ backend/src/main/java/com/helios/auctix/domain/User.java
@@ -0,0 +1,21 @@
+package com.helios.auctix.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "Users" )
+public class User {
+    @Id
+    private String id;
+    private String username;
+}
