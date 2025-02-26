4bb8f7371833a1440fd6ec431a0f662e00d836d6
Sakindu Ransindu <sakindu.ransindu@gmail.com>
2025-02-27 00:22:19 +0530

    add provider for redux

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,37 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import ProtectedRoute from './components/auth/ProtectedRoute';
-
-
-import LoginPage from './pages/Login';
-import CreateBidPage from './pages/create-bid';
+import LoginPage from '@/pages/Login';
+import Register from '@/pages/Register';
+import ProtectedRoute from '@/components/auth/ProtectedRoute';
 import Home from './pages/Home';
-import Register from './pages/Register';
-
+import { Provider } from 'react-redux';
+import { store } from './services/store';
+import Dashboard from './pages/Dashboard';
 
 const App: React.FC = () => {
   return (
-    <AuthProvider>
+    <Provider store={store}>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<LoginPage />} />
-
           <Route path="/register" element={<Register />} />
-
-          <Route path="/create-bid" element={<CreateBidPage />} />{' '}
-
           <Route
             path="/dashboard"
             element={
-              <ProtectedRoute allowedUsers={['SELLER']} redirectPath="/login">
-                {' '}
-                <Dashboard />{' '}
+              <ProtectedRoute
+                allowedUsers={['SELLER', 'BIDDER']}
+                redirectPath="/login"
+              >
+                <Dashboard />
               </ProtectedRoute>
             }
           />
+          <Route path="/403" element={<h2>403 Unautherized</h2>} />
         </Routes>
       </Router>
-    </AuthProvider>
+    </Provider>
   );
 };
-
-const Dashboard: React.FC = () => <h2>Dashboard</h2>;
 
 export default App;
