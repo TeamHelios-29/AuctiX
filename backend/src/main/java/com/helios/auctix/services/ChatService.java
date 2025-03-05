6cd7c41c0cf900cad71fc183ee4fd095b163b266
Sachintha Lakmin <68807141+sachintha-lk@users.noreply.github.com>
2025-03-05 15:59:17 +0530

    feat(chat): Add websocket configs

---
+++ backend/src/main/java/com/helios/auctix/services/ChatService.java
@@ -0,0 +1,13 @@
+package com.helios.auctix.services;
+
+import com.helios.auctix.domain.chat.ChatMessage;
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Log
+@Service
+public class ChatService {
+
+}
