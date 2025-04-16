15c6e7c5a4109fc3c15abd02682ad5544d297cf8
itstharinatalphagenes <tharin@alphagenes.site>
2025-04-16 22:12:58 +0530

    auction-details page frontend created & added to app route

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -11,6 +11,7 @@ import Home from '@/pages/Home';
 import CreateAuction from '@/pages/create-auction';
 import SellerProfile from '@/pages/SellerProfile';
 import AuctionChat from '@/components/organisms/auction-chat';
+import AuctionDetailsPage from '@/pages/auction-details';
 
 export default function AppRouter() {
   return (
@@ -39,7 +40,8 @@ export default function AppRouter() {
         {/* Other Routes */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/create-bid" element={<CreateAuction />} />{' '}
+        <Route path="/create-auction" element={<CreateAuction />} />
+        <Route path="/auction-details" element={<AuctionDetailsPage />} />
         <Route path="/403" element={<h2>403 Unautherized</h2>} />
         <Route path="/test-chat" element={<AuctionChat />} />
         <Route path="/user" element={<User />} />
