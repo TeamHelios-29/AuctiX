c94041950c1b7be7bf917a7e613d85ec689d96da
garuka-satharasinghe <methman2002@gmail.com>
2025-07-17 20:29:48 +0530

    Update UI components: change font to Geist, enhance AuctionCard styling, implement SellerReport dialog, and refactor Navbar and SellerProfile layout

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,6 +6,7 @@ import {
   AccordionTrigger,
   AccordionContent,
 } from '@/components/ui/Accordion';
+import { Link } from 'react-router-dom';
 import { Textarea } from '@/components/ui/textarea';
 import { ReactNode } from 'react';
 
@@ -42,23 +43,25 @@ export default function Home(): ReactNode {
                 </span>
               </div>
               <div className="items-center mt-6 sm:mt-8">
-                <Button className="w-48">
-                  Explore Auctions{' '}
-                  <svg
-                    width="16"
-                    height="16"
-                    viewBox="0 0 16 16"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M6 5.33333H10.6667M5.33333 8H9.33333M7.33333 10.6667H10.6667M3.33333 2H12.6667C13.403 2 14 2.59695 14 3.33333V12.6667C14 13.403 13.403 14 12.6667 14H3.33333C2.59695 14 2 13.403 2 12.6667V3.33333C2 2.59695 2.59695 2 3.33333 2Z"
-                      stroke="#ffffffff"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                  </svg>
-                </Button>
+                <Link to="/explore-auctions">
+                  <Button className="w-48">
+                    Explore Auctions{' '}
+                    <svg
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M6 5.33333H10.6667M5.33333 8H9.33333M7.33333 10.6667H10.6667M3.33333 2H12.6667C13.403 2 14 2.59695 14 3.33333V12.6667C14 13.403 13.403 14 12.6667 14H3.33333C2.59695 14 2 13.403 2 12.6667V3.33333C2 2.59695 2.59695 2 3.33333 2Z"
+                        stroke="#ffffffff"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                  </Button>
+                </Link>
               </div>
             </div>
           </div>
